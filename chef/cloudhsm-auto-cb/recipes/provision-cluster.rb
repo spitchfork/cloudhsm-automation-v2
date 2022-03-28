9c3f23c9ddf06223d0a521cff5d07f2b8c40accf
Stephen Pitchfork <s.pitchfork@googlemail.com>
2022-03-28 11:30:27 +0100

    Added security group conf and create hsm.

--- chef/cloudhsm-auto-cb/recipes/provision-cluster.rb
+++ chef/cloudhsm-auto-cb/recipes/provision-cluster.rb
@@ -6,8 +6,26 @@
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
 
+# Create the CloudHSM cluster synchronously and wait for completion
+# NOTE this can take up to 5 minutes to complete.
 execute 'create_cluster' do
   command node['cloudhsm_auto']['py_cloudhsm_utils_create_cmd']
+  cwd node['cloudhsm_auto']['py_cloudhsm_utils_dir']
+  live_stream true
+  action :run
+end
+
+# Configure comms between the EC2 client and HSM security groups
+execute 'conf_security_groups' do
+  command node['cloudhsm_auto']['py_cloudhsm_utils_conf_secgrp_cmd']
+  cwd node['cloudhsm_auto']['py_cloudhsm_utils_dir']
+  live_stream true
+  action :run
+end
+
+# Create a single HSM in any given AZ
+execute 'create_hsm' do
+  command node['cloudhsm_auto']['py_cloudhsm_utils_create_hsm_cmd']
   cwd node['cloudhsm_auto']['py_cloudhsm_utils_dir']
   live_stream true
   action :run
